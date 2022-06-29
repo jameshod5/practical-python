9951bbe17b1c1285fb08b32f0a24eaa5c418a454
xq0472 <james.hodson@ukaea.uk>
2022-06-29 10:34:03 +0100

    Works out bounce height of a ball dropped 100m, after 10 bounces.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+
+bounce_height = 100
+num_bounces = 1
+
+while num_bounces < 11:
+    bounce_height = bounce_height * (3/5)
+    num_bounces = num_bounces + 1
+    print(num_bounces, bounce_height)
