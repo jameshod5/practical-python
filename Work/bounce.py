74c744c2967102d90ad9b737e9702a4826a27216
xq0472 <james.hodson@ukaea.uk>
2022-06-29 10:36:47 +0100

    Fixed issue with number of bounces counted.

--- Work/bounce.py
+++ Work/bounce.py
@@ -4,9 +4,9 @@
 
 
 bounce_height = 100
-num_bounces = 1
+num_bounces = 0
 
-while num_bounces < 11:
+while num_bounces < 10:
     bounce_height = bounce_height * (3/5)
     num_bounces = num_bounces + 1
     print(num_bounces, bounce_height)
