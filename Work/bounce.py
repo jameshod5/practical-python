537ab9a59d0e3d5c66816f195badac35b78b106f
xq0472 <james.hodson@ukaea.uk>
2022-06-29 10:39:05 +0100

    Added round function to remove a few decimals

--- Work/bounce.py
+++ Work/bounce.py
@@ -7,6 +7,6 @@ bounce_height = 100
 num_bounces = 0
 
 while num_bounces < 10:
-    bounce_height = bounce_height * (3/5)
+    bounce_height = round(bounce_height * (3/5), 4)
     num_bounces = num_bounces + 1
     print(num_bounces, bounce_height)
