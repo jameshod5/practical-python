9d25a85c54ac3f15b17674def4c2751474ec48d6
xq0472 <james.hodson@ukaea.uk>
2022-06-29 10:28:08 +0100

    Added input for name, and change to format of answer

--- Work/sears.py
+++ Work/sears.py
@@ -4,13 +4,17 @@ sears_height = 442 #metres
 number_of_bills = 1
 day = 1
 
+name = input("What is your name?")
+print("Your name is", name)
+
+
 while number_of_bills * bill_thickness < sears_height:
     height = (bill_thickness*number_of_bills)
     day = day + 1
     number_of_bills = number_of_bills*2
 
 
-print("The number of bills exceeds the height!")
+print("The number of bills exceeds the height after:")
 print("Number of days", day)
     
     
